4bdefd33c7c559e4fde30802f50a6d1502f8d2b1
Felipe Silva <felipe.silva@webtech87.pt>
2025-03-10 16:28:11 +0000

    [FIX] Add STATICFILES_STORAGE to settings.py

--- psycology/settings.py
+++ psycology/settings.py
@@ -76,6 +76,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = [
