379ffb5f6ed27ebc1ea2a2457c716d097e105062
Felipe Silva <felipe.silva@webtech87.pt>
2025-03-11 16:49:52 +0000

    [UPDATE] Merged forms.py, test.css, and test.html from test-responsive branch

--- main/forms.py
+++ main/forms.py
@@ -13,24 +13,27 @@ class ContactRequestForm(forms.Form):
     full_name = forms.CharField(
         label='Nome:',
         max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control white-border', 'placeholder': 'Insira o seu nome', 'style': 'border-radius: 8px;'})
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form-control white-border', 'placeholder': 'Insira o seu nome', 'style': 'border-radius: 8px;'})        
         )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': 'form-control white-border', 'placeholder': 'Insira o seu email', 'style': 'border-radius: 8px;'})
+        label='Email:',
+        required=False,
+        widget=forms.EmailInput(attrs={'class': 'form-control white-border', 'placeholder': 'Insira o seu email', 'style': 'border-radius: 8px;'})        
     )
     subject = forms.ChoiceField(
         label='Assunto:',
         choices = Subjects.choices,
-        required=True,
         initial= Subjects.SELECT,
-        widget=forms.Select(attrs={'class': 'form-control white-border', 'style': 'border-radius: 8px;'})
+        widget=forms.Select(attrs={'class': 'form-control white-border', 'style': 'border-radius: 8px;'})        
     )
     message = forms.CharField(
         label='Mensagem:',
-        widget=forms.Textarea(attrs={'class': 'form-control white-border', 'rows': 5, 'cols': 40, 'placeholder': 'Escreva aqui a sua mensagem', 'style': 'border-radius: 8px;'})
+        required=False,
+        widget=forms.Textarea(attrs={'class': 'form-control white-border', 'rows': 5, 'cols': 40, 'placeholder': 'Escreva aqui a sua mensagem', 'style': 'border-radius: 8px;'})        
     )
     accept_terms = forms.BooleanField(
-        label='',
-        required=True,
-        widget=forms.CheckboxInput(attrs={'class': 'form-check-input', 'id': 'exampleCheck'})
+        label='',        
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input', 'id': 'exampleCheck'})        
     )
